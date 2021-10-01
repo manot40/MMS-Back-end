7a3bffcd5d0ec0f79d34120a25ce960a9bb90127
manot40 <kevinsandiho@outlook.com>
2021-10-01 21:49:09 +0700

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-require("dotenv").config()
-import log from "./helpers/pino"
-import Server from "./server"
+require("dotenv").config();
+import log from "./helpers/pino";
+import Server from "./server";
 
-new Server()
+new Server();
 
 process.on("uncaughtException", (error: Error) => {
-  log.error("Uncaught Exception | " + error.message)
-})
+  log.error("Uncaught Exception | " + error.message);
+});
 
 process.on("unhandledRejection", (error: Error) => {
-  log.error("Unhandled Promise Rejection | " + error)
-})
+  log.error("Unhandled Promise Rejection | " + error);
+});
 
 module.exports = Server;
