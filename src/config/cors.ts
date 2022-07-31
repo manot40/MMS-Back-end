8c4fc50851f7020402c3d260aecabd7f7c50352e
manot40 <kevinsandiho290100@gmail.com>
2022-07-31 15:34:51 +0700

    fix: cors separation

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,6 +1,6 @@
 require('dotenv').config();
 // CORS Settings
-let whiteList: Array<string> | string = process.env.CORS_WHITELIST.split(';');
+let whiteList: Array<string> | string = process.env.CORS_WHITELIST.split('_');
 if (!whiteList[0]) whiteList = '*';
 
 const corsConfig = {
